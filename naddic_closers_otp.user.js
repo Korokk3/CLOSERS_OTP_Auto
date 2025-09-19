9fda7dd778d3d4112ea4bb91c5f305f95918836c
Korokke <korokke@xtr.kr>
2025-09-19 19:37:51 +0900

    feat: 확장프로그램 내부 스토리지 이용

--- naddic_closers_otp.user.js
+++ naddic_closers_otp.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         Naddic Closers OTP
 // @namespace    https://www.naddic.co.kr
-// @version      2025-09-19
+// @version      2025-09-19v2
 // @description  클로저스 공식 홈페이지 (나딕게임즈) OTP 자동 입력
 // @author       Korokk3
 // @match        https://www.naddic.co.kr/ko/web/member/login*
 // @match        https://www.naddic.co.kr/ko/service/otp*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=naddic.co.kr
-// @grant        none
+// @grant        GM.setValue
+// @grant        GM.getValue
 // @require      https://raw.githubusercontent.com/jiangts/JS-OTP/refs/heads/master/js/sha_dev.js
 // @require      https://raw.githubusercontent.com/jiangts/JS-OTP/refs/heads/master/js/jsOTP.js
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
     console.log("Naddic OTP 자동입력 스크립트가 실행되었습니다.");
@@ -20,68 +21,74 @@
     const custom_alert = window.showCustomAlert || alert;
     const vars = {
         otp: new jsOTP.totp(30, 6), // OTP 값은 30초마다 갱신, 6자리의 코드
-        get key() { return localStorage.getItem(`OTP_PASS_KEY_${this.user_id}`) },
-        get user_id() { return localStorage.getItem("OTP_PASS_LAST_ID") }
-    }
-
-    // 마지막으로 로그인할 때 사용한 아이디 기록
-    if (document.getElementById("Login")) {
-        document.getElementById("Login").addEventListener("submit", (event) => {
-            localStorage.setItem("OTP_PASS_LAST_ID", document.querySelector("[name='userID']").value);
-        });
+        get key() { return this.user_id.then(user_id => { Promise.resolve(GM.getValue(`OTP_PASS_KEY_${user_id}`, null)) }); },
+		set key(v) { return this.user_id.then(user_id => { Promise.resolve(GM.setValue(`OTP_PASS_KEY_${user_id}`, v)) }); },
+        get user_id() { return Promise.resolve( GM.getValue("OTP_PASS_LAST_ID", null) ); },
+        set user_id(v) { return Promise.resolve( GM.setValue("OTP_PASS_LAST_ID", v) ); },
     }
 
     // OTP 값 저장
-    function set_otp(key) {
+    async function set_otp(key) {
         if (location.hash != "#step-2") return; // step-1과 step-2에서 나오는 키 값이 각각 다른데 step-2에서 나오는게 실제 값
         if (!key) return; // OTP 키 값이 없는 경우 패스
-        if (!vars.user_id) { // 아이디 기록을 찾을 수 없는 경우 수동 입력
-            while (!vars.user_id || vars.user_id.length < 7) {
+        while (1) {
+            let id = await vars.user_id
+            console.log(id);
+
+            if (!id || id.length < 7) {
                 let user_id = prompt("아이디를 찾을 수 없습니다.\n현재 로그인 중인 나딕게임즈ID (이메일)을 입력해주세요.");
-                if (user_id) localStorage.setItem("OTP_PASS_LAST_ID", user_id);
-            }
+                if (user_id) await (vars.user_id = user_id);
+            } else break;
         }
 
-        localStorage.setItem(`OTP_PASS_KEY_${vars.user_id}`, key);
+        await (vars.key = key);
         custom_alert(`현재 로그인 중인 계정의 OTP 인증 키가 ${key}(으)로 설정되었습니다.`);
     }
 
     // DOM 감시
     const observer = new MutationObserver(function(ml, ob) {
-        for (const m of ml){
-            // 로그인 OTP 값 자동 입력 및 로그인 버튼 자동 클릭
-            if (m.target.id === "otpWrap") {
-                if (m.attributeName === "style" && m.target.style.display !== "none" && vars.key) {
-                    let v = vars.otp.getOtp(vars.key, new Date().getTime());
-                    document.getElementById("validCode").value = v;
-                    submitOtpValidation();
+        vars.key.then(key => {
+            for (const m of ml){
+                // 로그인 OTP 값 자동 입력 및 로그인 버튼 자동 클릭
+                if (m.target.id === "otpWrap") {
+                    if (m.attributeName === "style" && m.target.style.display !== "none" && key) {
+                        let v = vars.otp.getOtp(key, new Date().getTime());
+                        document.getElementById("validCode").value = v;
+                        submitOtpValidation();
+                    }
+                }
+                // OTP 값 재확인 자동 입력
+                else if (m.target.id === "step-3") {
+                    if (m.attributeName === "style" && m.target.style.display === "block" && key) {
+                        let v = vars.otp.getOtp(key, new Date().getTime());
+                        document.getElementById("validCode").value = v;
+                        document.getElementById("validCode").type = "";
+                    }
+                }
+                // OTP 생성시 키 값 확인 및 저장
+                else {
+                    if (m.type === "characterData" && m.target.textContent) {
+                        console.info("OTP 코드를 발견했습니다. (Case 1)")
+                        set_otp(m.target.textContent);
+                    } else if (m.type == "childList" && m.target.innerText) {
+                        console.info("OTP 코드를 발견했습니다. (Case 2)")
+                        set_otp(m.target.textContent);
+                    }
                 }
             }
-            // OTP 값 재확인 자동 입력
-            else if (m.target.id === "step-3") {
-                if (m.attributeName === "style" && m.target.style.display === "block" && vars.key) {
-                    let v = vars.otp.getOtp(vars.key, new Date().getTime());
-                    document.getElementById("validCode").value = v;
-                    document.getElementById("validCode").type = "";
-                }
-            }
-            // OTP 생성시 키 값 확인 및 저장
-            else {
-                if (m.type === "characterData" && m.target.textContent) {
-                    console.log("OTP 코드를 발견했습니다. (Case 1)")
-                    set_otp(m.target.textContent, vars.user_id);
-                } else if (m.type == "childList" && m.target.innerText) {
-                    console.log("OTP 코드를 발견했습니다. (Case 2)")
-                    set_otp(m.target.textContent, vars.user_id);
-                }
-            }
-        }
+        });
     });
 
+    // 마지막으로 로그인할 때 사용한 아이디 기록
+    if (document.getElementById("Login")) {
+        document.getElementById("Login").addEventListener("submit", async (e) => {
+            await (vars.user_id = document.querySelector("[name='userID']").value);
+        });
+    }
 
     // validCode 자동 채우기
-    if (document.getElementById("validCode") && vars.key) {
-        let v = vars.otp.getOtp(vars.key, new Date().getTime());
+    if (document.getElementById("validCode") && await vars.key) {
+        let v = vars.otp.getOtp(await vars.key, new Date().getTime());
         document.getElementById("validCode").value = v;
         document.getElementById("validCode").type = "";
     }
